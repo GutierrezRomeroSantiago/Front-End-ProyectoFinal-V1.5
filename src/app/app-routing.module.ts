b5307b74930205d0a1ab80f71f24cbf1edbb52b8
unknown <sgutrom143@g.educaand.es>
2022-02-08 14:34:27 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrearPedidoComponent } from './components/crear-pedido/crear-pedido.component';
+import { ListarPedidoComponent } from './components/listar-pedido/listar-pedido.component';
+import { ListarPrendaComponent } from './components/listar-prenda/listar-prenda.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListarPedidoComponent},
+  { path: 'crearped', component: CrearPedidoComponent},
+  {path: 'listarprend/:id', component: ListarPrendaComponent},
+  { path: '**', redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListarPedidoComponent } from './components/listar-pedido/listar-pedido.component';
+import { CrearPedidoComponent } from './components/crear-pedido/crear-pedido.component';
+import { ListarPrendaComponent } from './components/listar-prenda/listar-prenda.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarPedidoComponent,
+    CrearPedidoComponent,
+    ListarPrendaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
